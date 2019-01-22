ee8dcab317e3ed0e515b071d1000df98fb9fea74
underwjr <underwjr@rose-hulman.edu>
2019-01-22 05:36:09 -0500

    Done with m2 2a

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Jairyq Underwood.
+"""  # Done 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# Done 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -19,16 +19,14 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 
 import rosegraphics as rg
 
+import math
+
 
 def main():
-    """
-    TESTS the functions that you will write below.
-    You write the tests per the _TODO_s below.
-    """
-
+    print(z(3, 4))
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# Done 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -41,6 +39,11 @@ def main():
 #
 ###############################################################################
 
+
+def z(x, y):
+
+    w = math.sqrt(x ** 2 + y ** 2)
+    return w
 
 ###############################################################################
 # TODO: 4a.  Define a function immediately below this _TODO_.
