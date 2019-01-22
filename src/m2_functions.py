fa4621aa8b9167b37d29e384a375e6b26cfc8f94
underwjr <underwjr@rose-hulman.edu>
2019-01-22 05:36:47 -0500

    Done through m2 2b

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -34,7 +34,7 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# Done 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
